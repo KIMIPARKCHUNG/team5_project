6ff96951d958145e141d6d90feeb77e0fd53f0ee
tulipksh <tulipksh@gmail.com>
2023-05-12 09:37:00 +0900

    로그인, 메일함, 메일본문 템플릿, 컨트롤러 추가

---
+++ step10_MVC_file_ing/src/main/java/com/spring/mvc/EmailListController.java
@@ -0,0 +1,31 @@
+package com.spring.mvc;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.dto.Emp;
+import com.spring.service.EmpService;
+
+@Controller
+public class EmailListController {
+	
+	@Autowired
+	EmpService empService;
+	
+	@RequestMapping(value = "/emailList", method = RequestMethod.GET)
+	public String emailList() {
+		return "emailList";
+	}
+	
+}
