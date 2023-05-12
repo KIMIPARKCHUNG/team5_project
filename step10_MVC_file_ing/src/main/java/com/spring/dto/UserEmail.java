63fa785f485c9547a7c905960df58e3757e1b17d
LeeDaehong <mlskboy@naver.com>
2023-05-12 10:28:50 +0900

    UserEmail에 대한 DTO 파일

---
+++ step10_MVC_file_ing/src/main/java/com/spring/dto/UserEmail.java
@@ -0,0 +1,19 @@
+package com.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserEmail {
+	private String email_address;
+	private String mem_name;
+	private String password;
+}
